39133154570092625c3f8e674b20690a8223f38f
Sujal-Gupta-SG <Sujal24092004+github@gmail.com>
2024-09-17 09:04:55 +0530

    Release Update

--- server.js
+++ server.js
@@ -16,9 +16,16 @@ const dbName = process.env.DB_NAME;
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
+
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
+
+
+
 
 app.get("/", async (req, res) => {
   try {
